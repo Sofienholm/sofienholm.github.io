d89ed3f49bf8fb0a3b7ff33935261be0cc806a49
Sofienholm <eaa24snho@students.eaaa.dk>
2025-09-08 09:03:25 +0200

    m

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,8 @@
-import ProjektSection from "./components/ProjektSection";
-export default function Projects() {
+export function Projects() {
   return (
-    <section className="Clients">
+    <div>
       <h1>About Page</h1>
       <p>This is the about page of the application.</p>
-      <ProjektSection />
-    </section>
+    </div>
   );
 }
