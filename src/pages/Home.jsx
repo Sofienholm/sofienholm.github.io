ab46f7deea33331714ac48d315a59985e6dcefca
Sofienholm <eaa24snho@students.eaaa.dk>
2025-09-12 21:49:02 +0200

    f

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import styles from "./Home.module.css";
+import img1 from "../assets/bike.jpg";
+import img2 from "../assets/portrait.jpg";
+import img3 from "../assets/notebook.jpg";
+import img4 from "../assets/laptop.jpg";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hej – jeg er Sofie</h1>
-      <p>Multimediedesign • Frontend • React</p>
-    </>
+    <section className={styles.hero}>
+      <h1 className={styles.title}>Sofie Neergaard</h1>
+      <img src={img1} className={styles.img1} alt="" />
+      <img src={img2} className={styles.img2} alt="" />
+      <img src={img3} className={styles.img3} alt="" />
+      <img src={img4} className={styles.img4} alt="" />
+      <p className={styles.year}>PORTFOLIO 2025</p>
+      <p>Scroll ned og se, hvad jeg kan</p>
+    </section>
   );
 }
