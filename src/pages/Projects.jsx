646a97463a79ba9bba4e7794ada8746dcc7d9314
Sofienholm <eaa24snho@students.eaaa.dk>
2025-09-12 20:52:36 +0200

    b

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,36 @@
+<<<<<<< HEAD
 export function Projects() {
+=======
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router";
+
+export default function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("/data/projects.json");
+      const data = await response.json();
+      setProjects(data);
+    }
+    getData();
+  }, []);
+>>>>>>> 46a9cf209e0722a2f52f011d0570f5f8cd3e974a
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page of the application.</p>
-    </div>
+    <section id="projects">
+      <div className="headline-container">
+        <h2>Projects</h2>
+      </div>
+      <div className="grid">
+        {projects.map((project) => (
+          <article key={project.id} className="card">
+            <img src={project.image} alt={project.title} />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <NavLink to={`/projects/${project.id}`}>Se detaljer</NavLink>
+          </article>
+        ))}
+      </div>
+    </section>
   );
 }
